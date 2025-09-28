e22fda9bdc62141fa3d084a8869068fead1cafd4
kash-suredyn <157395527+kash-suredyn@users.noreply.github.com>
2025-09-28 14:11:54 -0400

    Add react-components-export/index.js

---
+++ react-components-export/index.js
@@ -0,0 +1,45 @@
+// SureDyn React Components Export
+// Main entry point for all components
+
+// Layout Components
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as Layout } from './Layout';
+
+// Main Page Components  
+export { default as HomePage } from './HomePage';
+export { default as AboutPage } from './AboutPage';
+export { default as ContactPage } from './ContactPage';
+export { default as ServicePage } from './ServicePage';
+
+// Service Page Components
+export { default as ImplementationPage } from './services/ImplementationPage';
+export { default as UpgradePage } from './services/UpgradePage';
+export { default as SupportPage } from './services/SupportPage';
+export { default as CustomDevelopmentPage } from './services/CustomDevelopmentPage';
+export { default as TrainingPage } from './services/TrainingPage';
+export { default as BusinessIntelligencePage } from './services/BusinessIntelligencePage';
+
+// Industry Page Components
+export { default as ManufacturingPage } from './industries/ManufacturingPage';
+export { default as DistributionPage } from './industries/DistributionPage';
+export { default as RetailPage } from './industries/RetailPage';
+export { default as HealthcarePage } from './industries/HealthcarePage';
+export { default as ProfessionalServicesPage } from './industries/ProfessionalServicesPage';
+export { default as FinancialServicesPage } from './industries/FinancialServicesPage';
+
+// Solution Page Components
+export { default as Dynamics365Page } from './solutions/Dynamics365Page';
+export { default as PowerBIPage } from './solutions/PowerBIPage';
+export { default as PowerPlatformPage } from './solutions/PowerPlatformPage';
+export { default as AzurePage } from './solutions/AzurePage';
+
+// Common Components
+export { default as Hero } from './Hero';
+export { default as CTASection } from './CTASection';
+export { default as FeatureGrid } from './FeatureGrid';
+export { default as ProcessSteps } from './ProcessSteps';
+export { default as ContactForm } from './ContactForm';
+
+// Export examples
+export { default as Examples } from './examples/BasicUsage';
